c63dd4c5bc997f3a1b79aa93f1cee16f7c5bdf83
Patrick Göttsch <patrick.goettsch@tuhh.de>
2023-12-06 19:00:50 +0100

    Some formating and ordering

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,52 @@
-#include <WiFi.h>
 #include <AsyncTCP.h>
-#include <WiFiManager.h>
 #include <ESPAsyncWebServer.h>
-#include <Preferences.h>
 #include <Logging.h>
 #include <ModbusBridgeWiFi.h>
 #include <ModbusClientRTU.h>
+#include <Preferences.h>
+#include <WiFiManager.h>
+#include <WiFi.h>
+
 #include "config.h"
 #include "pages.h"
 
 AsyncWebServer webServer(80);
 Config config;
 Preferences prefs;
-ModbusClientRTU *MBclient;
 ModbusBridgeWiFi MBbridge;
+ModbusClientRTU *MBclient;
 WiFiManager wm;
 
-void setup() {
+void setup()
+{
   debugSerial.begin(115200);
   dbgln();
-  dbgln("[config] load")
+  dbgln("[config] load");
   prefs.begin("modbusRtuGw");
   config.begin(&prefs);
   debugSerial.end();
   debugSerial.begin(config.getSerialBaudRate(), config.getSerialConfig());
+
   dbgln("[wifi] start");
   WiFi.mode(WIFI_STA);
   wm.setClass("invert");
   auto reboot = false;
-  wm.setAPCallback([&reboot](WiFiManager *wifiManager){reboot = true;});
+  wm.setAPCallback([&reboot](WiFiManager *wifiManager)
+                   { reboot = true; });
   wm.autoConnect();
-  if (reboot){
+  if (reboot)
+  {
     ESP.restart();
   }
   dbgln("[wifi] finished");
-  dbgln("[modbus] start");
 
+  dbgln("[modbus] start");
   MBUlogLvl = LOG_LEVEL_WARNING;
   RTUutils::prepareHardwareSerial(modbusSerial);
+  
 #if defined(RX_PIN) && defined(TX_PIN)
   // use rx and tx-pins if defined in platformio.ini
-  modbusSerial.begin(config.getModbusBaudRate(), config.getModbusConfig(), RX_PIN, TX_PIN );
+  modbusSerial.begin(config.getModbusBaudRate(), config.getModbusConfig(), RX_PIN, TX_PIN);
   dbgln("Use user defined RX/TX pins");
 #else
   // otherwise use default pins for hardware-serial2
@@ -53,7 +59,7 @@ void setup() {
   for (uint8_t i = 1; i < 248; i++)
   {
     MBbridge.attachServer(i, i, ANY_FUNCTION_CODE, MBclient);
-  }  
+  }
   MBbridge.start(config.getTcpPort(), 10, config.getTcpTimeout());
   dbgln("[modbus] finished");
   setupPages(&webServer, MBclient, &MBbridge, &config, &wm);
@@ -61,6 +67,7 @@ void setup() {
   dbgln("[setup] finished");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
